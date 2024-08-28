822f6d66e606b865c33cb9148864b89a7811cf0d
Joseph Cavalcanti <43867250+Joetosh@users.noreply.github.com>
2024-08-27 21:26:37 -0300

    been there done that renamed stuff, fixed logic that made faulty compharisons, created an entire function for time stuff

--- ModEntry.cs
+++ ModEntry.cs
@@ -2,71 +2,75 @@
 using StardewModdingAPI.Events;
 using StardewValley;
 
-namespace TrueTime {
+namespace TrueTime;
+
 public sealed class ModConfig {
-
-  public Dictionary<ulong, DateTime> Data { get; set; }
-
-  public ModConfig() { this.Data = new Dictionary<ulong, DateTime>(); }
+  public Dictionary<ulong, DateTime> LogTimes { get; set; } = new();
 }
 
 internal sealed class ModEntry : Mod {
-  private ModConfig Config = new();
+  private ModConfig? _config = new();
+
   // public methods
   public override void Entry(IModHelper helper) {
     helper.Events.GameLoop.OneSecondUpdateTicked += OnUpdate;
-    helper.Events.GameLoop.SaveLoaded += OnSaveload;
-    helper.Events.GameLoop.Saved += OnSaved;
+    helper.Events.GameLoop.SaveLoaded += OnLoadSave;
+    helper.Events.GameLoop.Saved += OnSave;
   }
 
   // private methods
-  private void OnUpdate(object? sender, OneSecondUpdateTickedEventArgs e) {
+  private static void OnUpdate(object? sender,
+                               OneSecondUpdateTickedEventArgs e) {
     if (!Context.IsWorldReady)
       return;
-    Game1.timeOfDay = (DateTime.Now.Hour * 100) + DateTime.Now.Minute;
-    // TODO fix it later
-    if (Game1.timeOfDay >= 0200 || Game1.timeOfDay < 0600)
-      Game1.PassOutNewDay();
+
+    Game1.timeOfDay = DateTime.Now.Hour * 100 + DateTime.Now.Minute;
   }
-  private void OnSaveload(object? sender, SaveLoadedEventArgs e) {
-    if (Game1.gameTimeInterval != 0)
-      Game1.gameTimeInterval = 0;
 
-    this.Config = this.Helper.ReadConfig<ModConfig>();
-    if (Config.Data.ContainsKey(Game1.uniqueIDForThisGame)) {
-      var TimeInterval =
-          DateTime.Now.Subtract(Config.Data[Game1.uniqueIDForThisGame]).Days;
-      if (TimeInterval > 0) {
-        Game1.year = TimeInterval / 112;
-        Game1.dayOfMonth = TimeInterval % 112;
+  private void OnLoadSave(object? sender, SaveLoadedEventArgs e) {
+    Game1.gameTimeInterval = 0;
 
-        Game1.addHUDMessage(new HUDMessage(
-            message: $"It's been {Game1.year} years and {Game1.dayOfMonth} days since you left the {Game1.player.farmName} farm."));
+    _config = Helper.ReadConfig<ModConfig>();
+    // checks if save hasn't time data
+    if (!_config.LogTimes.ContainsKey(Game1.uniqueIDForThisGame))
+      return;
+    var timeInterval =
+        DateTime.Now.Subtract(_config.LogTimes[Game1.uniqueIDForThisGame]).Days;
+    // check if at least a day hasn't passed
+    if (timeInterval < 0)
+      return;
+    TimeTravel(timeInterval);
+    Game1.addHUDMessage(new HUDMessage(
+        $"It's been {Game1.year} years and {Game1.dayOfMonth} days since you left the {Game1.player.farmName} farm."));
+  }
 
-        switch (TimeInterval % 112) {
-        case int n when (n <= 28):
-          Game1.season = Season.Spring;
-          break;
-        case int n when (n <= 56):
-          Game1.season = Season.Summer;
-          Game1.dayOfMonth -= 28;
-          break;
-        case int n when (n <= 84):
-          Game1.season = Season.Fall;
-          Game1.dayOfMonth -= 56;
-          break;
-        case int n when (n <= 112):
-          Game1.season = Season.Winter;
-          Game1.dayOfMonth -= 84;
-          break;
-        }
-        Game1.setGraphicsForSeason();
-      }
+  private void OnSave(object? sender, SavedEventArgs e) {
+    _config.LogTimes.Add(Game1.uniqueIDForThisGame, DateTime.Today.Date);
+    Helper.WriteConfig(_config);
+  }
+
+  private void TimeTravel(int interval) {
+    Game1.year = interval / 112;
+    var seasonIndex = Game1.dayOfMonth = interval % 112;
+
+    switch (seasonIndex) {
+    case int n when n <= 28:
+      Game1.season = Season.Spring;
+      break;
+    case int n when n <= 56:
+      Game1.season = Season.Summer;
+      Game1.dayOfMonth -= 28;
+      break;
+    case int n when n <= 84:
+      Game1.season = Season.Fall;
+      Game1.dayOfMonth -= 56;
+      break;
+    case int n when n <= 112:
+      Game1.season = Season.Winter;
+      Game1.dayOfMonth -= 84;
+      break;
     }
+
+    Game1.setGraphicsForSeason();
   }
-  private void OnSaved(object? sender, SavedEventArgs e) {
-    Config.Data.Add(Game1.uniqueIDForThisGame, DateTime.Today.Date);
-    this.Helper.WriteConfig(this.Config);
-  }
-}
 }
